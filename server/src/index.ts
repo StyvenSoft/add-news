6117946a337102774895c21eeb91235adf6f23dc
Steveen <steveen.echeverri@gmail.com>
2020-12-15 19:25:51 -0500

    Created first migration test/mikroORM

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,18 @@
-console.log("Welcome!");
+import { MikroORM } from '@mikro-orm/core';
+import { __prod__ } from './constants';
+import { Post } from './entities/Post';
+import microConfig from "./mikro-orm.config";
+
+const main = async () => {
+    const orm = await MikroORM.init(microConfig);
+
+    const post = orm.em.create(Post, {title: 'My title'});
+    await orm.em.persistAndFlush(post);
+    console.log("--sql2--");
+    await orm.em.nativeInsert(Post, {title: 'My title native'});
+    
+};
+
+main().catch((err) => {
+    console.log(err);
+});
