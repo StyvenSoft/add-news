d67bac910cc696651b66592ea0e67a155c56f4f0
Steveen <steveen.echeverri@gmail.com>
2020-12-18 16:24:05 -0500

    Created query for logged in

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -1,6 +1,6 @@
 import { User } from "../entities/User";
 import { MyContext } from "../types";
-import { Arg, Ctx, Field, InputType, Mutation, ObjectType, Resolver } from "type-graphql";
+import { Arg, Ctx, Field, InputType, Mutation, ObjectType, Query, Resolver } from "type-graphql";
 import argon2 from "argon2";
 
 @InputType()
@@ -31,10 +31,20 @@ class UserResponse {
 
 @Resolver()
 export class userResolver {
+    @Query(() => User, { nullable: true })
+    async me(@Ctx() { req, em }: MyContext ) {
+        // Your not logged in
+        if(!req.session.id) {
+            return null 
+        }
+        const user = await em.findOne(User, {id: req.session.userId} );
+        return user;
+    }
+
     @Mutation(() => UserResponse)
     async register(
         @Arg("options") options: UsernamePasswordInput,
-        @Ctx() { em }: MyContext
+        @Ctx() { em, req }: MyContext
     ): Promise<UserResponse> {
         if (options.username.length <= 2) {
             return {
@@ -77,6 +87,8 @@ export class userResolver {
                 };
             }
         }
+        
+        req.session.userId = user.id;
         return { user };
     }
 
