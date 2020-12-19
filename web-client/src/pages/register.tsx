62f8181925a7498d892fe633b3b13ddd3c519383
Steveen <steveen.echeverri@gmail.com>
2020-12-19 14:10:21 -0500

    Deleted components and change pages tsx

---
+++ web-client/src/pages/register.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+interface registerProps {
+
+}
+
+
+const register: React.FC<registerProps> = ({}) => {
+    return (
+        <div>Register page</div>
+    );
+}
+
+export default register;
