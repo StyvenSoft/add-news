7461d270dfda9668710278888db3e0dbf58dd61c
Steveen <steveen.echeverri@gmail.com>
2020-12-19 18:00:56 -0500

    Cretated inputFields component Form

--- web-client/src/pages/register.tsx
+++ web-client/src/pages/register.tsx
@@ -1,27 +1,41 @@
-import { FormControl, FormLabel, Input } from '@chakra-ui/react';
+import { Box, Button } from '@chakra-ui/react';
 import { Form, Formik } from 'formik';
 import React from 'react';
+import { InputField } from '../components/InputField';
+import { Wrapper } from '../components/Wrapper';
 
 interface registerProps { }
 
 
 const register: React.FC<registerProps> = ({ }) => {
     return (
-        <Formik initialValues={{ username: "", password: "" }}
-            onSubmit={(values) => {
-                console.log(values);
-            }}>
-            {(values, handleChange) => (
-                <Form>
-                    <FormControl>
-                        <FormLabel htmlFor="username">First name</FormLabel>
-                        <Input id="username" placeholder="Username"
-                                onchange={handleChange}
-                                value={values.username} />
-                    </FormControl>
-                </Form>
-            )}
-        </Formik>
+        <Wrapper variant="small">
+            <Formik initialValues={{ username: "", password: "" }}
+                onSubmit={(values) => {
+                    console.log(values);
+                }}>
+                {(props) => (
+                    <Form>
+                        <InputField
+                            name="username"
+                            placeholder="username"
+                            label="Username"
+                        />
+                        <Box mt={4}>
+                            <InputField
+                                name="password"
+                                placeholder="password"
+                                label="Password"
+                                type="password"
+                            />
+                        </Box>
+                        <Button mt={4} type="submit" isLoading={props.isSubmitting} colorScheme="teal">
+                            Register
+                        </Button>
+                    </Form>
+                )}
+            </Formik>
+        </Wrapper>
     );
 }
 
