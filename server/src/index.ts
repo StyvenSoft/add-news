8c3247a1c5aabefcd18c94c733c2c1ea28f48467
Steveen <steveen.echeverri@gmail.com>
2020-12-19 21:41:40 -0500

    congig URQL Basics completed

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,6 @@
 import 'reflect-metadata';
 import { MikroORM } from '@mikro-orm/core';
 import { __prod__ } from './constants';
-// import { Post } from './entities/Post';
 import microConfig from "./mikro-orm.config";
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
@@ -12,7 +11,7 @@ import { userResolver } from './resolvers/user';
 import redis from 'redis';
 import session from 'express-session';
 import connectRedis from 'connect-redis';
-import { MyContext } from './types';
+import cors from 'cors';
 
 const main = async () => {
     const orm = await MikroORM.init(microConfig);
@@ -22,6 +21,13 @@ const main = async () => {
 
     const RedisStore = connectRedis(session)
     const redisClient = redis.createClient()
+    
+    app.use(
+        cors({
+            origin: "http://localhost:3000",
+            credentials: true,
+        })
+    );
 
     app.use(
         session({
@@ -47,10 +53,13 @@ const main = async () => {
             resolvers: [testResolver, postResolver, userResolver],
             validate: false
         }),
-        context: ({ req, res }): MyContext => <MyContext>({em: orm.em, req, res}),
+        context: ({ req, res }) => ({em: orm.em, req, res}),
     });
 
-    apolloServer.applyMiddleware({ app });
+    apolloServer.applyMiddleware({ 
+        app,
+        cors: false,
+    });
 
     app.listen(PORT, () => {
         console.log(`Server started on http://localhost:${PORT}`);
--- web-client/src/pages/register.tsx
+++ web-client/src/pages/register.tsx
@@ -1,18 +1,34 @@
 import { Box, Button } from '@chakra-ui/react';
 import { Form, Formik } from 'formik';
 import React from 'react';
+import { useMutation } from 'urql';
 import { InputField } from '../components/InputField';
 import { Wrapper } from '../components/Wrapper';
 
 interface registerProps { }
 
+const REGISTER_MUT = `
+mutation Register($username: String!, $password: String!) {
+    register(options: { username: $username, password: $password }) {
+        errors {
+            field
+            message
+        }
+        user {
+            id
+            username
+        }
+    }
+}
+`
 
 const register: React.FC<registerProps> = ({ }) => {
+    const [, register] = useMutation(REGISTER_MUT);
     return (
         <Wrapper variant="small">
             <Formik initialValues={{ username: "", password: "" }}
                 onSubmit={(values) => {
-                    console.log(values);
+                    return register(values);
                 }}>
                 {(props) => (
                     <Form>
