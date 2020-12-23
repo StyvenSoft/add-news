93193e922f4d885e8ab98c2a8d31da5f56454060
Steveen <steveen.echeverri@gmail.com>
2020-12-22 19:55:52 -0500

    URQL Cache Updates

--- web-client/src/components/NavBar.tsx
+++ web-client/src/components/NavBar.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Box, Button, Flex } from '@chakra-ui/react';
 import NextLink from 'next/link';
+import { useMeQuery } from '../generated/graphql';
 
 interface NavBarProps { }
 
 export const NavBar: React.FC<NavBarProps> = ({ }) => {
-    return (
-        <Flex borderBottom="2px" borderColor="gray.200" p={4}>
-            <Box ml={"auto"}>
+    const [{ data, fetching }] = useMeQuery();
+    let body = null;
+
+    if (fetching) {
+
+    } else if (!data?.me) {
+        body = (
+            <>
                 <NextLink href="/login">
                     <Button mr={2} colorScheme="teal" variant="solid">
                         Login
@@ -18,6 +24,25 @@ export const NavBar: React.FC<NavBarProps> = ({ }) => {
                         Register
                     </Button>
                 </NextLink>
+            </>
+        )
+    } else {
+        body = (
+            <Box>
+                <Button mr={2} colorScheme="teal" variant="ghost">
+                    {data.me.username}
+                </Button>
+                <Button colorScheme="teal" variant="solid">
+                    Logout
+                </Button>
+            </Box>
+        )
+    }
+
+    return (
+        <Flex borderBottom="2px" borderColor="gray.200" p={4}>
+            <Box ml={"auto"}>
+                {body}
             </Box>
         </Flex>
     )
