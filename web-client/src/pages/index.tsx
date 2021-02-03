b980f03c07f1163242689036f7eda0d528415113
Steveen <steveen.echeverri@gmail.com>
2021-02-02 22:21:32 -0500

    Create resolver new forupdatePost

--- web-client/src/pages/index.tsx
+++ web-client/src/pages/index.tsx
@@ -36,32 +36,32 @@ const Index = () => {
         <div>Loading...</div>
       ) : (
           <Stack spacing={8}>
-            {data!.posts.posts.map((p) => 
+            {data!.posts.posts.map((p) =>
               !p ? null : (
-              <Flex key={p.id} p={5} shadow="md" borderWidth="1px">
-                <UpdootSection post={p} />
-                <Box flex={1}>
-                  <NextLink href="/post/[id]" as={`/post/${p.id}`}>
-                    <Link>
-                      <Heading fontSize="xl">{p.title}</Heading>
-                    </Link>
-                  </NextLink>
-                  <Text>Posted by <strong>{p.creator.username}</strong> {format(p.createdAt)}</Text>
-                  <Flex>
-                    <Text flex={1} mt={4}>{p.textSnippet}</Text>
-                    <IconButton 
-                      ml="auto" 
-                      aria-label="Search database" 
-                      icon={<DeleteIcon w={6} h={6} />}
-                      colorScheme="red"
-                      onClick={() => {
-                        deletePost({ id: p.id })
-                      }}
-                    />
-                  </Flex>
-                </Box>
-              </Flex>
-            ))}
+                <Flex key={p.id} p={5} shadow="md" borderWidth="1px">
+                  <UpdootSection post={p} />
+                  <Box flex={1}>
+                    <NextLink href="/post/[id]" as={`/post/${p.id}`}>
+                      <Link>
+                        <Heading fontSize="xl">{p.title}</Heading>
+                      </Link>
+                    </NextLink>
+                    <Text>Posted by <strong>{p.creator.username}</strong> {format(p.createdAt)}</Text>
+                    <Flex>
+                      <Text flex={1} mt={4}>{p.textSnippet}</Text>
+                      <IconButton
+                        ml="auto"
+                        aria-label="Search database"
+                        icon={<DeleteIcon w={6} h={6} />}
+                        colorScheme="red"
+                        onClick={() => {
+                          deletePost({ id: p.id })
+                        }}
+                      />
+                    </Flex>
+                  </Box>
+                </Flex>
+              ))}
           </Stack>
         )}
       {data && data.posts.hasMore ? (
