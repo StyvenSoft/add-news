45cbb0e7c8d0b3d4750d9412144b53dac2b153e7
Steveen <steveen.echeverri@gmail.com>
2021-02-04 22:14:44 -0500

    Complete temp update post mutation

--- web-client/src/pages/post/edit/[id].tsx
+++ web-client/src/pages/post/edit/[id].tsx
@@ -1,21 +1,46 @@
 import { Box, Button, Heading } from "@chakra-ui/react";
-import { Formik, Form } from "formik";
-import { withUrqlClient } from "next-urql"
+import { Form, Formik } from "formik";
+import { withUrqlClient } from "next-urql";
+import { useRouter } from "next/router";
 import React from "react";
 import { InputField } from "../../../components/InputField";
 import { Layout } from "../../../components/Layout";
-import { createUrqlClient } from "../../../utils/createUrqlClient"
+import { usePostQuery, useUpdatePostMutation } from "../../../generated/graphql";
+import { createUrqlClient } from "../../../utils/createUrqlClient";
+import { useGetIntId } from "../../../utils/useGetIntId";
 
 const EditPost = ({ }) => {
+    const router = useRouter();
+    const intId = useGetIntId();
+    const [{ data, fetching }] = usePostQuery({
+        pause: intId === -1,
+        variables: {
+            id: intId,
+        }
+    });
+    const [, updatePost] = useUpdatePostMutation();
+
+    if (fetching) {
+        return (
+            <Layout>Loading...</Layout>
+        )
+    }
+
+    if (!data?.post) {
+        return (
+            <Layout>
+                <Box>Could not find news</Box>
+            </Layout>
+        )
+    }
+
     return (
         <Layout variant='small'>
             <Heading fontSize="xl" mb={4}>Edit News</Heading>
-            <Formik initialValues={{ title: "", text: "" }}
-                onSubmit={() => {                  
-                    // const {error} = await createPost({ input: values });
-                    // if (!error) {
-                    //     router.push("/");
-                    // }
+            <Formik initialValues={{ title: data.post.title, text: data.post.text }}
+                onSubmit={async (values) => {                  
+                    await updatePost({ id: intId, ...values })
+                    router.back();
                 }}>
                 {(props) => (
                     <Form>
